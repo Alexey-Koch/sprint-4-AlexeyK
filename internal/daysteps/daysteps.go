54d3b4416b2c1a4df736715a7f31f6e0f9494096
Alexey Kochurov <Koalva13@yandex.ru>
2025-03-09 18:07:10 +0300

    daysteps-commit-1

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,12 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/go1fl-4-sprint-final/internal/spentcalories"
 )
 
 var (
@@ -9,7 +14,31 @@ var (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// ваш код ниже
+	// Разделяем строку на слайс.
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, fmt.Errorf("недопустимый формат данных")
+	}
+
+	// Преобразуем количество шагов в int.
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка преобразования количества шагов: %v", err)
+	}
+
+	// Проверяем, что количество шагов больше 0.
+	if steps <= 0 {
+		return 0, 0, fmt.Errorf("количество шагов должно быть больше 0")
+	}
+
+	// Преобразуем продолжительность в time.Duration.
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка преобразования продолжительности: %v", err)
+	}
+
+	return steps, duration, nil
+
 }
 
 // DayActionInfo обрабатывает входящий пакет, который передаётся в
@@ -19,5 +48,25 @@ func parsePackage(data string) (int, time.Duration, error) {
 // Если пакет валидный, он добавляется в слайс storage, который возвращает
 // функция. Если пакет невалидный, storage возвращается без изменений.
 func DayActionInfo(data string, weight, height float64) string {
-	// ваш код ниже
+	// Получаем количество шагов и продолжительность прогулки.
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	// Проверяем, чтобы количество шагов было больше 0.
+	if steps <= 0 {
+		return ""
+	}
+
+	// Вычисляем дистанцию в метрах и переводим в километры.
+	distanceMeters := float64(steps) * StepLength
+	distanceKilometers := distanceMeters / 1000
+
+	// Вычисляем количество калорий, потраченных на прогулке.
+	calories := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+
+	// Формируем строку для возврата.
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", steps, distanceKilometers, calories)
 }
